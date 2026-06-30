eead6531abb46255346094dd96276b8b5ee005e9
alfakeysara <alfakeysara@gmail.com>
2026-06-30 14:22:42 -0400

    Refactor console logs to use variables

--- jsdemo.js
+++ jsdemo.js
@@ -1,4 +1,7 @@
 console.log("This is loaded from the external js");
-console.log("Name: Sara Alfakey");
-console.log("School: Philip O Berry");
-console.log("Hobby: Listening To Music");
+var name = "Sara Alfakey";
+var school = "Philip O Berry";
+let hobby = "Listening to Music";
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Hobby: ${hobby}`);
