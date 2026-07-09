4ba2681805cb3152cd24a3ebbde9284fa603e7ae
alfakeysara <alfakeysara@gmail.com>
2026-07-09 13:43:01 -0400

    Initialize travel map with coordinates and marker

---
+++ travelmap.js
@@ -0,0 +1,8 @@
+//35.6762° N latitude and 139.6503° E longitude
+var coordinates = [35.6762°, 139.6503];
+var map = L.map('map').setView(coordinates, 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker(coordinates).bindPopup("<p>Hello, From Japan!</p>").addTo(map);
